ea922e88aa1ea018a082e24ffc53e59128e99bd2
Web3 Senpai⚡💛 <sareahj.com@gmail.com>
2025-01-14 02:10:11 +0100

    Update env.ts

--- server/src/utils/env.ts
+++ server/src/utils/env.ts
@@ -2,12 +2,14 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-export const DB_URL = process.env.DB_URL;
+const DB_URL = process.env.DB_URL;
 
-export const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-export const JWT_SECRET = process.env.JWT_SECRET || "secret";
+const JWT_SECRET = process.env.JWT_SECRET || "secret";
 
-export const ENVIRONMENT = process.env.NODE_ENV || "development";
+const ENVIRONMENT = process.env.NODE_ENV || "development";
 
-export const SOLANA_RPC = process.env.SOLANA_RPC || "https://api.devnet.solana.com";
+const SOLANA_RPC = process.env.SOLANA_RPC || "https://api.devnet.solana.com";
+
+export { JWT_SECRET, PORT, DB_URL, ENVIRONMENT, SOLANA_RPC };
